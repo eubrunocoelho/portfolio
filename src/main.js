e6371dd5a8f3a2684586593af588b3c0ff6f36ff
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-02 12:05:54 -0300

    chore: organizes imports

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
-import { createApp } from 'vue';
-import App from './App.vue';
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
-import { fas } from '@fortawesome/free-solid-svg-icons';
 import { fab } from '@fortawesome/free-brands-svg-icons';
+import { fas } from '@fortawesome/free-solid-svg-icons';
 import { far } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { createApp } from 'vue';
 
+import App from './App.vue';
 import './assets/scss/styles.scss';
 
 const IoniconsModuleScript = document.createElement('script');
