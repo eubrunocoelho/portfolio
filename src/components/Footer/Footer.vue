8c56f49cccd70e26922fc83a199e6db14f761453
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-02 11:57:44 -0300

    style(mobile): add style to top of footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,6 +1,21 @@
 <template>
     <footer>
-        <span style="color: #fff">FOOTER</span>
+        <div class="footer-top">
+            <ul class="social">
+                <li>
+                    <a href="#" class="social-box"><ion-icon name="logo-github" class="social-icon"></ion-icon></a>
+                </li>
+                <li>
+                    <a href="#" class="social-box"><ion-icon name="logo-linkedin" class="social-icon"></ion-icon></a>
+                </li>
+                <li>
+                    <a href="#" class="social-box"><ion-icon name="logo-deviantart" class="social-icon"></ion-icon></a>
+                </li>
+                <li>
+                    <a href="#" class="social-box"><ion-icon name="logo-discord" class="social-icon"></ion-icon></a>
+                </li>
+            </ul>
+        </div>
     </footer>
 </template>
 
