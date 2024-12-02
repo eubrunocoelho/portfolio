49ee3eb42393f1accb3c4b9eb58559c4d211e2ac
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-02 13:39:50 -0300

    style: adds style to presentation content

--- src/components/Presentation/Presentation.vue
+++ src/components/Presentation/Presentation.vue
@@ -1,14 +1,16 @@
 <template>
     <section class="presentation">
-        <hgroup>
-            <h1>Lorem Ipsum</h1>
-            <h2>Lorem ipsum dolor</h2>
-        </hgroup>
-        <p class="text">
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
-            dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
-        </p>
-        <button class="btn btn-primary">Solicitar Serviço</button>
+        <div class="wrapper">
+            <hgroup>
+                <h1>Lorem Ipsum</h1>
+                <h2>Lorem ipsum dolor</h2>
+            </hgroup>
+            <p class="text">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+                dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+            </p>
+            <button class="btn btn-primary btn--custom-width">Solicitar Serviço</button>
+        </div>
     </section>
 </template>
 
