1bfa2485acb04c08aa051fa15e55f39561192351
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-02 11:27:13 -0300

    style: add style to header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -5,6 +5,28 @@
                 <font-awesome-icon :icon="['fas', 'bars']" class="open-menu--icon" />
             </button>
             <div class="logo-wrapper"></div>
+            <nav class="navigation">
+                <ul>
+                    <li>
+                        <a href="javascript:void(0);">Início</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);">Sobre</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);">Serviços</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);">Projetos</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);">Tecnologias</a>
+                    </li>
+                    <li>
+                        <a href="javascript:void(0);">Contato</a>
+                    </li>
+                </ul>
+            </nav>
         </div>
     </header>
 </template>
