37478d501255572c56caff6640a9c9b6f6c9691f
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-02 13:19:34 -0300

    style(mobile): adds style to presentation content

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,18 @@
 <template>
     <VueHeader></VueHeader>
+    <main>
+        <VuePresentation></VuePresentation>
+    </main>
     <VueFooter></VueFooter>
 </template>
 
 <script>
 import VueHeader from './components/Header/Header.vue';
+import VuePresentation from './components/Presentation/Presentation.vue';
 import VueFooter from './components/Footer/Footer.vue';
 
 export default {
     name: 'App',
-    components: { VueHeader, VueFooter },
+    components: { VueHeader, VueFooter, VuePresentation },
 };
 </script>
---
+++ src/components/Presentation/Presentation.vue
@@ -0,0 +1,23 @@
+<template>
+    <section class="presentation">
+        <hgroup>
+            <h1>Lorem Ipsum</h1>
+            <h2>Lorem ipsum dolor</h2>
+        </hgroup>
+        <p class="text">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et
+            dolore magna aliqua. Quis ipsum suspendisse ultrices gravida.
+        </p>
+        <button class="btn btn-primary">Solicitar Serviço</button>
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'VuePresentation',
+};
+</script>
+
+<style lang="scss">
+@import './presentation';
+</style>
