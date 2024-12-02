5e445e61d58d1bfd99f0630953100209c68f0c3e
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-02 12:05:40 -0300

    style: adjust style and apply breakpoints

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,20 +1,30 @@
 <template>
     <footer>
         <div class="footer-top">
-            <ul class="social">
-                <li>
-                    <a href="#" class="social-box"><ion-icon name="logo-github" class="social-icon"></ion-icon></a>
-                </li>
-                <li>
-                    <a href="#" class="social-box"><ion-icon name="logo-linkedin" class="social-icon"></ion-icon></a>
-                </li>
-                <li>
-                    <a href="#" class="social-box"><ion-icon name="logo-deviantart" class="social-icon"></ion-icon></a>
-                </li>
-                <li>
-                    <a href="#" class="social-box"><ion-icon name="logo-discord" class="social-icon"></ion-icon></a>
-                </li>
-            </ul>
+            <div class="container">
+                <ul class="social">
+                    <li>
+                        <a href="#" class="social-box">
+                            <ion-icon name="logo-github" class="social-icon"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="social-box">
+                            <ion-icon name="logo-linkedin" class="social-icon"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="social-box">
+                            <ion-icon name="logo-deviantart" class="social-icon"></ion-icon>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#" class="social-box">
+                            <ion-icon name="logo-discord" class="social-icon"></ion-icon>
+                        </a>
+                    </li>
+                </ul>
+            </div>
         </div>
     </footer>
 </template>
