60ebdc5a05388362b7d5e44af70bb7dde0ba19f8
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-03 16:16:17 -0300

    style: adds style to project content

--- src/components/Projects/Projects.vue
+++ src/components/Projects/Projects.vue
@@ -1,21 +1,29 @@
 <template>
     <section class="projects">
         <h1 class="section-title title-spacing">Projetos<span class="dot">.</span></h1>
-        <div class="project-item">
-            <span class="project-box"></span>
-            <div class="about">
-                <h1 class="title">Página Pessoal Minimalista</h1>
-                <p class="text">
-                    Lorem ipsum odor amet, consectetuer adipiscing elit. Volutpat eget massa varius primis elit
-                    hendrerit.
-                </p>
-                <ul class="languages">
-                    <li class="language-box language-box--vue"></li>
-                    <li class="language-box language-box--js"></li>
-                    <li class="language-box language-box--css"></li>
-                    <li class="language-box language-box--sass"></li>
-                </ul>
-                <a href="#" class="show-more">Exibir mais</a>
+        <div class="container">
+            <div class="previous">
+                <font-awesome-icon :icon="['fas', 'chevron-left']" class="previous--icon" />
+            </div>
+            <div class="next">
+                <font-awesome-icon :icon="['fas', 'chevron-right']" class="next--icon" />
+            </div>
+            <div class="project-item">
+                <span class="project-box"></span>
+                <div class="about">
+                    <h1 class="title">Página Pessoal Minimalista</h1>
+                    <p class="text">
+                        Lorem ipsum odor amet, consectetuer adipiscing elit. Volutpat eget massa varius primis elit
+                        hendrerit.
+                    </p>
+                    <ul class="languages">
+                        <li class="language-box language-box--vue"></li>
+                        <li class="language-box language-box--js"></li>
+                        <li class="language-box language-box--css"></li>
+                        <li class="language-box language-box--sass"></li>
+                    </ul>
+                    <a href="#" class="show-more">Exibir mais</a>
+                </div>
             </div>
         </div>
         <div class="ellipses">
