58b2e46f7fd585a5497873309e21763394c67df3
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-03 17:57:42 -0300

    style: adds style to information content

--- src/components/Contact/Contact.vue
+++ src/components/Contact/Contact.vue
@@ -2,26 +2,35 @@
     <section class="contact">
         <h1 class="section-title title-spacing">Contato<span class="dot">.</span></h1>
         <div class="wrapper">
-            <div class="form">
-                <form>
-                    <div class="form-group">
-                        <label for="name">Nome</label>
-                        <input type="text" name="name" placeholder="Digite seu nome" />
+            <form>
+                <div class="form-group">
+                    <label for="name">Nome</label>
+                    <input type="text" name="name" placeholder="Digite seu nome" />
+                </div>
+                <div class="form-group">
+                    <label for="email">E-mail</label>
+                    <input type="text" name="email" placeholder="Digite seu e-mail" />
+                </div>
+                <div class="form-group">
+                    <label for="subject">Assunto</label>
+                    <input type="text" name="subject" placeholder="Digite um assunto" />
+                </div>
+                <div class="form-group">
+                    <label for="message">Mensagem</label>
+                    <textarea name="message" placeholder="Digite sua mensagem"></textarea>
+                </div>
+                <button class="btn btn-primary">Enviar Mensagem</button>
+            </form>
+            <div class="info">
+                <div class="info-card">
+                    <div class="icon-box">
+                        <font-awesome-icon :icon="['fab', 'whatsapp']" class="icon-box--icon" />
                     </div>
-                    <div class="form-group">
-                        <label for="email">E-mail</label>
-                        <input type="text" name="email" placeholder="Digite seu e-mail" />
+                    <div class="about">
+                        <h1 class="title">WhatsApp</h1>
+                        <span class="description">(41) 98738-9397</span>
                     </div>
-                    <div class="form-group">
-                        <label for="subject">Assunto</label>
-                        <input type="text" name="subject" placeholder="Digite um assunto" />
-                    </div>
-                    <div class="form-group">
-                        <label for="message">Mensagem</label>
-                        <textarea name="message" placeholder="Digite sua mensagem"></textarea>
-                    </div>
-                    <button class="btn btn-primary">Enviar Mensagem</button>
-                </form>
+                </div>
             </div>
         </div>
     </section>
