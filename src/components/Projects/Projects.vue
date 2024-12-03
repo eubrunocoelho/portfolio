da14dfa11da37ec1613791c1b8e2da829954717b
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-03 15:08:35 -0300

    style(mobile): adds style to project content

--- src/components/Projects/Projects.vue
+++ src/components/Projects/Projects.vue
@@ -1,15 +1,29 @@
 <template>
     <section class="projects">
         <h1 class="section-title title-spacing">Projetos<span class="dot">.</span></h1>
-        <div class="content">
+        <div class="project-item">
             <span class="project-box"></span>
-            <h1>Página Pessoal Minimalista</h1>
+            <h1 class="title">Página Pessoal Minimalista</h1>
             <div class="about">
                 <p class="text">
                     Lorem ipsum odor amet, consectetuer adipiscing elit. Volutpat eget massa varius primis elit
                     hendrerit.
                 </p>
+                <ul class="languages">
+                    <li class="language-box language-box--vue"></li>
+                    <li class="language-box language-box--js"></li>
+                    <li class="language-box language-box--css"></li>
+                    <li class="language-box language-box--sass"></li>
+                </ul>
                 <a href="#" class="show-more">Exibir mais</a>
+            </div>
+        </div>
+        <div class="ellipses">
+            <div class="wrapper">
+                <span class="ellipse"></span>
+                <span class="ellipse"></span>
+                <span class="ellipse ellipse--active"></span>
+                <span class="ellipse"></span>
             </div>
         </div>
     </section>
