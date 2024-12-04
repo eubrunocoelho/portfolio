d0aadd50287968d6bb3d5501bbeef771e347c7e5
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-04 13:30:22 -0300

    style: organizes style

--- src/components/Technologies/Technologies.vue
+++ src/components/Technologies/Technologies.vue
@@ -1,69 +1,69 @@
 <template>
     <section class="technologies">
-        <h1 class="section-title title-spacing">Tecnologias<span class="dot">.</span></h1>
+        <h1 class="main-title main-title--spacing">Tecnologias<span class="dot">.</span></h1>
         <div class="wrapper">
-            <div class="technology-card">
+            <div class="technology">
                 <h1 class="title">Front-End</h1>
                 <div class="container">
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/html.svg" class="language-img" />
+                        <img src="../../assets/img/languages/html.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/css.svg" class="language-img" />
+                        <img src="../../assets/img/languages/css.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/js.svg" class="language-img" />
+                        <img src="../../assets/img/languages/js.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/jquery.svg" class="language-img" />
+                        <img src="../../assets/img/languages/jquery.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/vuejs.svg" class="language-img" />
+                        <img src="../../assets/img/languages/vuejs.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/sass.svg" class="language-img" />
+                        <img src="../../assets/img/languages/sass.svg" class="language" />
                     </div>
                 </div>
             </div>
-            <div class="technology-card">
+            <div class="technology">
                 <h1 class="title">Back-End</h1>
                 <div class="container">
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/php.svg" class="language-img" />
+                        <img src="../../assets/img/languages/php.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/nodejs.svg" class="language-img" />
+                        <img src="../../assets/img/languages/nodejs.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/ts.svg" class="language-img" />
+                        <img src="../../assets/img/languages/ts.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/graphql.svg" class="language-img" />
+                        <img src="../../assets/img/languages/graphql.svg" class="language" />
                     </div>
                 </div>
             </div>
-            <div class="technology-card">
+            <div class="technology">
                 <h1 class="title">Frameworks</h1>
                 <div class="container">
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/laravel.svg" class="language-img" />
+                        <img src="../../assets/img/languages/laravel.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/vuejs.svg" class="language-img" />
+                        <img src="../../assets/img/languages/vuejs.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/jquery.svg" class="language-img" />
+                        <img src="../../assets/img/languages/jquery.svg" class="language" />
                     </div>
                 </div>
             </div>
-            <div class="technology-card">
+            <div class="technology">
                 <h1 class="title">Banco de Dados</h1>
                 <div class="container">
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/mysql.svg" class="language-img" />
+                        <img src="../../assets/img/languages/mysql.svg" class="language" />
                     </div>
                     <div class="technology-box">
-                        <img src="../../assets/img/languages/mongodb.svg" class="language-img" />
+                        <img src="../../assets/img/languages/mongodb.svg" class="language" />
                     </div>
                 </div>
             </div>
