4a21f969c59411d3ce31412f4a128c829b9c63d4
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-17 13:52:46 -0300

    feat: separate alert component and add custom messages

---
+++ src/components/Contact/partial/AlertMessage.vue
@@ -0,0 +1,21 @@
+<template>
+    <transition name="fadeAlert">
+        <slot></slot>
+    </transition>
+</template>
+
+<script>
+export default {
+    name: 'AlertMessage',
+};
+</script>
+
+<style>
+.fadeAlert-enter-active {
+    animation: fadeAlert 0.5s ease-in-out;
+}
+
+.fadeAlert-leave-active {
+    animation: fadeAlert 0.5s ease-in-out reverse;
+}
+</style>
--- src/components/Contact/partial/Form.vue
+++ src/components/Contact/partial/Form.vue
@@ -2,24 +2,66 @@
     <form class="form form_reveal" autocomplete="off" @submit.prevent="submitForm">
         <div class="form-group">
             <label for="name">Nome</label>
-            <input type="text" name="name" placeholder="Digite seu nome" v-model="state.name" @blur="v$.name.$touch" />
-            <transition name="fade">
+            <input
+                type="text"
+                name="name"
+                id="name"
+                placeholder="Digite seu nome"
+                v-model="state.name"
+                @blur="v$.name.$touch"
+            />
+            <alertMessage>
                 <div v-if="v$.name.$error" class="alert alert-danger margin--top-20">
                     <p>{{ v$.name.$errors[0].$message }}</p>
                 </div>
-            </transition>
+            </alertMessage>
         </div>
         <div class="form-group">
             <label for="email">E-mail</label>
-            <input type="text" name="email" placeholder="Digite seu e-mail" />
+            <input
+                type="text"
+                name="email"
+                id="email"
+                placeholder="Digite seu e-mail"
+                v-model="state.email"
+                @blur="v$.email.$touch"
+            />
+            <alertMessage>
+                <div v-if="v$.email.$error" class="alert alert-danger margin--top-20">
+                    <p>{{ v$.email.$errors[0].$message }}</p>
+                </div>
+            </alertMessage>
         </div>
         <div class="form-group">
             <label for="subject">Assunto</label>
-            <input type="text" name="subject" placeholder="Digite um assunto" />
+            <input
+                type="text"
+                name="subject"
+                id="subject"
+                placeholder="Digite um assunto"
+                v-model="state.subject"
+                @blur="v$.subject.$touch"
+            />
+            <alertMessage>
+                <div v-if="v$.subject.$error" class="alert alert-danger margin--top-20">
+                    <p>{{ v$.subject.$errors[0].$message }}</p>
+                </div>
+            </alertMessage>
         </div>
         <div class="form-group">
             <label for="message">Mensagem</label>
-            <textarea name="message" placeholder="Digite sua mensagem"></textarea>
+            <textarea
+                name="message"
+                id="message"
+                placeholder="Digite sua mensagem"
+                v-model="state.message"
+                @blur="v$.message.$touch"
+            ></textarea>
+            <alertMessage>
+                <div v-if="v$.message.$error" class="alert alert-danger margin--top-20">
+                    <p>{{ v$.message.$errors[0].$message }}</p>
+                </div>
+            </alertMessage>
         </div>
         <button class="btn btn-primary button_reveal">Enviar Mensagem</button>
     </form>
@@ -32,17 +74,34 @@
 <script>
 import { reactive } from 'vue';
 import { useVuelidate } from '@vuelidate/core';
-import { required } from '@vuelidate/validators';
+import { required, helpers } from '@vuelidate/validators';
+
+import AlertMessage from './AlertMessage.vue';
 
 export default {
     name: 'ContactForm',
+    components: { AlertMessage },
     setup() {
         const state = reactive({
             name: '',
+            email: '',
+            subject: '',
+            message: '',
         });
 
         const rules = {
-            name: { required },
+            name: {
+                required: helpers.withMessage('Por favor, insira seu nome.', required),
+            },
+            email: {
+                required: helpers.withMessage('Por favor, insira seu endereço de e-mail.', required),
+            },
+            subject: {
+                required: helpers.withMessage('Por favor, insira um assunto.', required),
+            },
+            message: {
+                required: helpers.withMessage('Por favor, escreva uma mensagem.', required),
+            },
         };
 
         const v$ = useVuelidate(rules, state);
@@ -57,25 +116,3 @@ export default {
     },
 };
 </script>
-
-<style>
-.fade-enter-active {
-    animation: fade 0.5s ease-in-out;
-}
-
-.fade-leave-active {
-    animation: fade 0.5s ease-in-out reverse;
-}
-
-@keyframes fade {
-    from {
-        opacity: 0;
-        transform: translateY(-10px);
-    }
-
-    to {
-        opacity: 1;
-        transform: translateY(0);
-    }
-}
-</style>
