8bbac74647b96f03168b6aff61bf91f7db84d171
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-21 14:32:56 -0300

    refactor: inject ionicons script into main.js

--- src/main.js
+++ src/main.js
@@ -16,4 +16,11 @@ dom.watch();
 const app = createApp(App);
 app.config.ignoredElements = [/^ion-/];
 app.component('font-awesome-icon', FontAwesomeIcon);
+
+// IonIcons Inject
+const scriptIonIcons = document.createElement('script');
+scriptIonIcons.type = 'module';
+scriptIonIcons.src = 'https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js';
+document.head.appendChild(scriptIonIcons);
+
 app.mount('#app');
