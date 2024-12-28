8c97d87939f112740ff1bc753b870c5918f65fe3
Bruno Coelho <eu.brunocoelho94@gmail.com>
2024-12-28 14:22:02 -0300

    chore: update email api url

--- src/services/emailAPI.js
+++ src/services/emailAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const http = axios.create({ baseURL: 'http://localhost:3000/' });
+const http = axios.create({ baseURL: 'https://nodemailer-nestjs-api.vercel.app/' });
 
 http.interceptors.response.use(
     (response) => {
